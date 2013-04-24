28cfc76fb01ccddd81b7bb34f27e526588b66246
lfreitas <lfreitas@college.harvard.edu>
2013-04-24 10:45:10 -0400

    added kk.py

---
+++ kk.py
@@ -0,0 +1,13 @@
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+""  Coded with love by Emi Nietfeld and Lucas Freitas  ""
+""  CS124 - Programming Assignment 3                   ""
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+def kk () :
+	print "shit this works!"
+
+def main ():
+	kk()
+
+if __name__ == "__main__":
+	main()

eb9ed00299fdce09439e21839132b9e4d0655226
lfreitas <lfreitas@college.harvard.edu>
2013-04-28 14:50:05 -0400

    everything seems to work. check simulated annealing though

--- kk.py
+++ kk.py
@@ -8,82 +8,139 @@
 from utils import *
 from generate_input import *
 import math
+import heapq
+import operator
 max_iter = 25000
 
 def karmarkar_karp(input):
-	original = list(input)
-	while len(original) > 1:
-		distance = abs(original[0] - original[1])
-		del original[:2]
-		reverse_insort(original, distance)
-	print "Karmarkar-Karp: " + str(original[0])
+	h = []
+	for x in input:
+		heapq.heappush(h, -x)
+	while len(h) > 1:
+		a = abs(heapq.heappop(h))
+		b = abs(heapq.heappop(h))
+		dist = abs(a - b)
+		heapq.heappush(h, dist)
+	return heapq.heappop(h)
 
-def repeated_random (input): 
-	original = list(input)
-	iterations = max_iter
-	solution = map(lambda x: x * random.randrange(-1, 2, 2), original)
+def repeated_random (input):
+	# get a random solution and calculate its residual
+	solution = [x * random.randrange(-1, 2, 2) for x in input]
 	residual = abs(sum(solution))
+
+	# try to find better solutions
+	iterations = max_iter
 	while iterations > 0 and residual > 0:
-		new_solution = map(lambda x: x * random.randrange(-1, 2, 2), original)
+		# find new solution
+		new_solution = [x * random.randrange(-1, 2, 2) for x in input]
 		new_residual = abs(sum(new_solution))
+		
+		# check if solution is better
 		if new_residual < residual:
-			solution = new_solution
 			residual = new_residual
 		iterations -= 1
-	print "Repeated random: " + str(residual)
+	return residual
 
 def hill_climbing(input):
-	original = list(input)
-	iterations = max_iter
-	length = len(original)
-	solution = map(lambda x: x * random.randrange(-1, 2, 2), original)
+	# get a random solution and calculate its residual
+	solution = [x * random.randrange(-1, 2, 2) for x in input]
 	residual = sum(solution)
+
+	# length of the input and number of iterations
+	length = len(input)
+	iterations = max_iter
+
+	# try to find better solutions
 	while iterations > 0 and abs(residual) > 0:
-		[i, j] = random.sample(range(length), 2) # pick 2 different indices
+		# pick two different indices
+		[i, j] = random.sample(range(length), 2)
+		# flip sign of first index
 		new_residual = residual - 2 * solution[i]
-		x = random.randrange(-1, 2, 2)
-		new_residual = residual - 2 * solution[j] if x == -1 else residual
+		# flip sign of second with 0.5 probability
+		sign = random.randrange(-1, 2, 2)
+		new_residual = new_residual - 2 * solution[j] if sign == -1 else new_residual
+		# if the new residual is better, update solution
 		if abs(new_residual) < abs(residual):
 			solution[i] *= -1
-			solution[j] *= x
+			solution[j] *= sign
 			residual = new_residual
 		iterations -= 1
-	print "Hill climbing: " + str(abs(residual)) 
+	return abs(residual) 
 
 def T(iter):
 	return (10 ** 10) * (0.8 ** math.floor(iter / 300.))
 
-'''def annealing(input):
-	original = list(input)
-	iterations = max_iter
-	length = len(original)
-	solution = map(lambda x: x * random.randrange(-1, 2, 2), original)
-	best_solution = list(solution)
+def annealing(input):
+	# get a random solution and calculate its residual
+	solution = [x * random.randrange(-1, 2, 2) for x in input]
 	residual = sum(solution)
+
+	# length of the input, number of iterations and S''
+	length = len(input)
+	iterations = max_iter
+	best_solution = list(solution)
+
+	# try to find better solutions
 	while iterations > 0 and abs(residual) > 0:
-		[i, j] = random.sample(range(length), 2) # pick 2 different indices
+		# pick two different indices
+		[i, j] = random.sample(range(length), 2)
+		# flip sign of first index
 		new_residual = residual - 2 * solution[i]
-		x = random.randrange(-1, 2, 2)
-		new_residual = residual - 2 * solution[j] if x == -1 else residual
+		# flip sign of second with 0.5 probability
+		sign = random.randrange(-1, 2, 2)
+		new_residual = new_residual - 2 * solution[j] if sign == -1 else new_residual
+		# if the new residual is better, update solution
 		if abs(new_residual) < abs(residual):
 			solution[i] *= -1
-			solution[j] *= x
+			solution[j] *= sign
 			residual = new_residual
-			#print new_residual
 		else:
-			print math.exp(-(abs(new_residual) - abs(residual))/T(max_iter - iterations + 1))
+			# probability of moving
+			distance = abs(new_residual) - abs(residual)
+			t_iter = T(max_iter - iterations + 1)
+			probability = math.exp(-distance/t_iter)
+			if random.random() <= probability:
+				solution[i] *= -1
+				solution[j] *= sign
+		if abs(residual) < abs(sum(best_solution)):
+			best_solution = list(solution)
 
 		iterations -= 1
-	print "Simulated annealing: " + str(abs(residual)) '''
+	return abs(residual)
 
 def main():
-	random.seed()
-	generate_nums()
-	input = readInput("input.txt")
-	karmarkar_karp(input)
-	repeated_random(input)
-	hill_climbing(input)
-	#annealing(input)
+	kk = []
+	rr = []
+	hc = []
+	sa = []
+	tests = 50
+	print "Testing..."
+	for i in range(tests):
+		print "Test number " + str(i + 1)
+		generate_nums()
+		input = readInput("input.txt")
+		kk.append(karmarkar_karp(input))
+		rr.append(repeated_random(input))
+		hc.append(hill_climbing(input))
+		sa.append(annealing(input))
+
+	# print results in ranking order
+	ranking = {"Karmarkar-Karp": sum(kk)/tests, "Repeated random": sum(rr)/tests, "Hill climbing": sum(hc)/tests, "Simulated annealing": sum(sa)/tests}
+	ranking = sorted(ranking.iteritems(), key=operator.itemgetter(1))
+	
+	print "Results in decreasing performance order"
+	for method in ranking:
+		print method[0] + ": " + str(method[1])
+
+	print "\nKarmarkar-Karp\n"		
+	print kk
+	print "\nRepeated random\n"
+	print rr
+	print "\nHill climbing\n"
+	print hc
+	print "\nSimulated annealing\n"
+	print sa
+
 
 
 if __name__ == "__main__":

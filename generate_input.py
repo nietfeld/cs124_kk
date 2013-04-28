a3f1913c11b42265c853e617053c59f12c94d5c9
lfreitas <lfreitas@college.harvard.edu>
2013-04-28 12:18:37 -0400

    fixing repeated random, added annealing

--- generate_input.py
+++ generate_input.py
@@ -10,5 +10,6 @@ import random
 def generate_nums ():
 	random.seed()
 	file = open("input.txt", "w")
+	#file.write("10\n8\n7\n6\n5")
 	for i in range(100):
-		file.write(str(random.getrandbits(64)) + "\n")
+		file.write(str(random.randint(1, 10 ** 12)) + "\n")
--- kk.py
+++ kk.py
@@ -7,59 +7,83 @@
 
 from utils import *
 from generate_input import *
+import math
 max_iter = 25000
 
-def karmarkar_karp(list):
-	while len(list) > 1:
-		distance = abs(list[0] - list[1])
-		del list[:2]
-		reverse_insort(list, distance)
-	print "Karmarkar-Karp: " + str(list[0])
+def karmarkar_karp(input):
+	original = list(input)
+	while len(original) > 1:
+		distance = abs(original[0] - original[1])
+		del original[:2]
+		reverse_insort(original, distance)
+	print "Karmarkar-Karp: " + str(original[0])
 
-def repeated_random (list): 
-	solution = map(lambda x: x * random.randrange(-1, 2, 2), list)
-	residual = sum(solution)
-	print residual
-	for x in range(max_iter):
-		new_solution = map(lambda x: x * random.randrange(-1, 2, 2), list)
-		new_residual = sum(new_solution)
-		if abs(new_residual) < abs(residual): 
+def repeated_random (input): 
+	original = list(input)
+	iterations = max_iter
+	solution = map(lambda x: x * random.randrange(-1, 2, 2), original)
+	residual = abs(sum(solution))
+	while iterations > 0 and residual > 0:
+		new_solution = map(lambda x: x * random.randrange(-1, 2, 2), original)
+		new_residual = abs(sum(new_solution))
+		if new_residual < residual:
 			solution = new_solution
 			residual = new_residual
-		print residual
-	print "repeated_random"
-	print abs(residual)
+		iterations -= 1
+	print "Repeated random: " + str(residual)
 
-def hill_climbing(list):
-	# need to check on how this works.... 
-	solution = map(lambda x: x * random.randrange(-1, 2, 2), list)
+def hill_climbing(input):
+	original = list(input)
+	iterations = max_iter
+	length = len(original)
+	solution = map(lambda x: x * random.randrange(-1, 2, 2), original)
 	residual = sum(solution)
-	for all in range(max_iter):
-		[i, j] = random.sample(range(len(list)),2) # pick 2 different indices
+	while iterations > 0 and abs(residual) > 0:
+		[i, j] = random.sample(range(length), 2) # pick 2 different indices
+		new_residual = residual - 2 * solution[i]
 		x = random.randrange(-1, 2, 2)
-		if (x = -1): 
-			new_residual = (residual - (solution[i]+solution[j])*2) # flip the sign
-			if abs(new_residual) < abs(residual): 
-				residual = new_residual
-				solution[i] *= -1
-				solution[j] *= -1 
-		if (x = 1): 
-			new_residual = (residual - solution[i]*2) # flip the sign
-			if abs(new_residual) < abs(residual): 
-				residual = new_residual
-				solution[i] *= -1
-	print "hill_climbing:"
-	print abs(residual)
+		new_residual = residual - 2 * solution[j] if x == -1 else residual
+		if abs(new_residual) < abs(residual):
+			solution[i] *= -1
+			solution[j] *= x
+			residual = new_residual
+		iterations -= 1
+	print "Hill climbing: " + str(abs(residual)) 
 
-def annealing(list):
-	print "hi"
+def T(iter):
+	return (10 ** 10) * (0.8 ** math.floor(iter / 300.))
+
+'''def annealing(input):
+	original = list(input)
+	iterations = max_iter
+	length = len(original)
+	solution = map(lambda x: x * random.randrange(-1, 2, 2), original)
+	best_solution = list(solution)
+	residual = sum(solution)
+	while iterations > 0 and abs(residual) > 0:
+		[i, j] = random.sample(range(length), 2) # pick 2 different indices
+		new_residual = residual - 2 * solution[i]
+		x = random.randrange(-1, 2, 2)
+		new_residual = residual - 2 * solution[j] if x == -1 else residual
+		if abs(new_residual) < abs(residual):
+			solution[i] *= -1
+			solution[j] *= x
+			residual = new_residual
+			#print new_residual
+		else:
+			print math.exp(-(abs(new_residual) - abs(residual))/T(max_iter - iterations + 1))
+
+		iterations -= 1
+	print "Simulated annealing: " + str(abs(residual)) '''
 
 def main():
+	random.seed()
 	generate_nums()
 	input = readInput("input.txt")
 	karmarkar_karp(input)
 	repeated_random(input)
 	hill_climbing(input)
+	#annealing(input)
 
 
 if __name__ == "__main__":
